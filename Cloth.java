1f279dd21b6823a7160aaf7954d0326e1dd4574b
Tusharmarvick <123730348+Tusharmarvick@users.noreply.github.com>
2023-04-13 14:24:10 +0530

    Update Cloth.java

--- Cloth.java
+++ Cloth.java
@@ -5,15 +5,3 @@ String size;
 String colour;
 Car(int a,String b,String c)
 {
-cost=a;
-model=b;
-name=c;
-}
-public static void main(String[] args)
-{
-Car t1=new Car(1010,"x","blue");
-System.out.println(t1.cost+"");
-System.out.println(""+t1.model);
-System.out.println(t1.name);
-}
-}
